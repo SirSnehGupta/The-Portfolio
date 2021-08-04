4ed57b0d8bb08274d6ad5dad762e7bbbfe14e0a4
SirSnehGupta <guptasneh302004@gmail.com>
2021-08-04 12:53:56 +0530

    Mode Switcher final

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var mode=null
-
 var light = document.querySelector('#light')
 light.addEventListener('click', function light(){
     document.querySelector("head > link:nth-child(5)").href = 'styles.css'
-    mode = 'light'
+    document.cookie = 'light'
 })
 
 var dark = document.querySelector('#dark')
 dark.addEventListener('click', function dark(){
     document.querySelector("head > link:nth-child(5)").href = 'stylesd.css'
-    mode = 'dark'
+    document.cookie = 'dark'
 })
+
+if (document.cookie === 'light'){
+    document.querySelector("head > link:nth-child(5)").href = 'styles.css'
+}
+
+if (document.cookie === 'dark'){
+    document.querySelector("head > link:nth-child(5)").href = 'stylesd.css'
+}

5e388ec33f08e3b23d3ec12d46ad00a03d1de3f9
SirSnehGupta <guptasneh302004@gmail.com>
2022-01-28 10:14:40 +0530

    JS changes

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var light = document.querySelector('#light')
 light.addEventListener('click', function light(){
-    document.querySelector("head > link:nth-child(5)").href = 'styles.css'
+    document.querySelector("head > link").href = 'styles.css'
     document.cookie = 'light'
 })
 
 var dark = document.querySelector('#dark')
 dark.addEventListener('click', function dark(){
-    document.querySelector("head > link:nth-child(5)").href = 'stylesd.css'
+    document.querySelector("head > link").href = 'stylesd.css'
     document.cookie = 'dark'
 })
 
 if (document.cookie === 'light'){
-    document.querySelector("head > link:nth-child(5)").href = 'styles.css'
+    document.querySelector("head > link").href = 'styles.css'
 }
 
 if (document.cookie === 'dark'){
-    document.querySelector("head > link:nth-child(5)").href = 'stylesd.css'
+    document.querySelector("head > link").href = 'stylesd.css'
 }
